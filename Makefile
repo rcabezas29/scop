3da9c8f3e0b439a3e0e49e72f695af3bea217fdb
rcabezas29 <raul.29cg@gmail.com>
2025-05-24 19:51:02 +0200

    Hello World GLFW/GLAD window

--- Makefile
+++ Makefile
@@ -3,13 +3,13 @@ TARGET := scop
 BUILD_DIR := ./build
 SRC_DIRS := ./srcs
 
-SRCS := $(shell find $(SRC_DIRS) -name '*.cpp')
+SRCS := $(shell find $(SRC_DIRS) -name '*.cpp') glad/src/glad.c
 OBJS := $(SRCS:%=$(BUILD_DIR)/%.o)
 
-INC_DIRS := $(shell find $(SRC_DIRS) -type d)
+INC_DIRS := $(shell find $(SRC_DIRS) -type d) glfw/build/include glad/include
 INC_FLAGS := $(addprefix -I,$(INC_DIRS))
 
-CXX := clang++
+CXX := g++
 CXXFLAGS := $(INC_FLAGS) -Wall -Wextra -Werror
 
 all: $(BUILD_DIR)/$(TARGET)
@@ -17,9 +17,13 @@ all: $(BUILD_DIR)/$(TARGET)
 $(BUILD_DIR)/$(TARGET): $(OBJS)
 	cmake -S glfw -B glfw/build
 	make -C glfw/build
-	$(CXX) $(CXXFLAGS) $(OBJS) -o $@
+	$(CXX) $(CXXFLAGS) $(OBJS) -o $@ -l GL -l glfw -ldl -lm
 
 $(BUILD_DIR)/%.cpp.o: %.cpp
+	mkdir -p $(dir $@)
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+$(BUILD_DIR)/%.c.o: %.c
 	mkdir -p $(dir $@)
 	$(CXX) $(CXXFLAGS) -c $< -o $@
 
--- srcs/renderer/Renderer.cpp
+++ srcs/renderer/Renderer.cpp
@@ -1,8 +1,42 @@
 #include "Renderer.hpp"
 
+void	framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	(void)window;
+	glViewport(0, 0, width, height);
+}
+
 Renderer::Renderer()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return ;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		return ;
+	}
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return ;
 }
 
 Renderer::Renderer(const Renderer &copy)
